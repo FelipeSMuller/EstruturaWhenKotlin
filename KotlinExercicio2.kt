fea2045504d208af99a2dcdca2f1dff5bd1c3d60
Felipe Müller <felipesilvamuller@Hotmail.com>
2023-10-23 21:12:33 -0300

    Add files via upload

---
+++ KotlinExercicio2.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+	val diaSemana = 2;
+
+
+
+
+	when (diaSemana) {
+		
+		1 -> println("O dia escolhido $diaSemana é domingo")
+		2 -> println("O dia escolhido $diaSemana é segunda-feira")
+		3 -> println("O dia escolhido $diaSemana é terça-feira")
+		4 -> println("O dia escolhido $diaSemana é quarta-feira")
+		5 -> println("O dia escolhido $diaSemana é quinta-feira")
+		6 -> println("O dia escolhido $diaSemana é sexta-feira")
+		7 -> println("O dia escolhido $diaSemana é sábado")
+	}
+}
+
+
